ca49ad1c05ff3313055d3fe73837d5f1f58b6ee0
Arasia <rlacndtjs261@gmail.com>
2021-05-23 23:50:51 +0900

    refactor: ConfigurationProperties
    - Remove `@Component` in Configuration
    - Add `@ConfigurationPropertiesScan` in Application

--- ServiceServer2/src/main/java/com/example/config/ApplicationConfiguration.java
+++ ServiceServer2/src/main/java/com/example/config/ApplicationConfiguration.java
@@ -3,9 +3,7 @@ package com.example.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @Getter
 @Setter
 @ConfigurationProperties("application")
