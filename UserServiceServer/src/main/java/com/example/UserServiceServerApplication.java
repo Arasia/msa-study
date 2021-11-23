d4be10ec48f4062c641ed2d87622cd382d243d21
Arasia <rlacndtjs261@gmail.com>
2021-11-23 17:42:12 +0900

    fix: ServiceServer1 -> UserServiceServer로 변경 및 기능 추가

--- UserServiceServer/src/main/java/com/example/UserServiceServerApplication.java
+++ UserServiceServer/src/main/java/com/example/UserServiceServerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 @EnableEurekaClient
 @SpringBootApplication
-public class ServiceServerApplication {
+public class UserServiceServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServiceServerApplication.class, args);
+        SpringApplication.run(UserServiceServerApplication.class, args);
     }
 }
